847fbc7d07d284078e1850629caa6a06721e9ca3
Victor <me.victorhermes@gmail.com>
2019-08-14 20:20:21 -0300

    database

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '192.168.99.100',
+  username: 'postgres',
+  password: 'docker',
+  database: 'nodebackend',
+  define: {
+    timestamps: true,
+    underscored: true /* caixa baixa sem underlines (tabela_tabela ao invés de tabelaTabela) */,
+    underscoredAll: true /* mesma coisa, mas para colunas e relacionamentos */,
+  },
+};
