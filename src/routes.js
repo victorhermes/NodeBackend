e5517243b1e3754e0a32b2f211f232cecbbc6d65
Victor <me.victorhermes@gmail.com>
2019-08-14 21:17:29 -0300

    UserController

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Victor Hermes',
-    email: 'oi@moom.com.br',
-    password_hash: '123456789',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
