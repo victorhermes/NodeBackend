75b994f4a233ccb16b364d3e8be1a7a4db118102
Victor <me.victorhermes@gmail.com>
2019-08-14 21:29:20 -0300

    hash bcrypt

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -7,8 +7,11 @@ class UserController {
     if (userExists) {
       return res.status(400).json({ error: 'Usuário já existe!' });
     }
+
+    // const { id, name, email } = await User.create(req.body);
     const user = await User.create(req.body);
 
+    // return res.json({ id, name, email });
     return res.json(user);
   }
 }
