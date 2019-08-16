d72fbbd9bd43839d8ec21c13f1dfa703fdf7204c
Victor <me.victorhermes@gmail.com>
2019-08-15 22:55:32 -0300

    comands

--- comands.js
+++ comands.js
@@ -1,5 +1,5 @@
 // yarn sequelize db:migrate
 // yarn sequelize migration:create --name=create-users
-// yarn sequelize db:migrate:undo    :all
+// yarn sequelize db:migrate:undo   :all
 // sequelize is an ORM to Postgres
 // There is something wrong with password change
