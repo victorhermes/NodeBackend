219494ae73a61a1abec312a5f8485341d47b9138
Victor <me.victorhermes@gmail.com>
2019-08-18 19:19:40 -0300

    providers list

--- src/app/controllers/ProviderController.js
+++ src/app/controllers/ProviderController.js
@@ -1,9 +1,17 @@
 import User from '../models/User';
+import File from '../models/File';
 
 class ProviderController {
   async index(req, res) {
     const providers = await User.findAll({
       where: { provider: true },
+      attributes: ['id', 'name', 'email', 'avatar_id'],
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+        },
+      ],
     });
 
     return res.json(providers);
