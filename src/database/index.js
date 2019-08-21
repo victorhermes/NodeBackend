afa1f3e337b50e62e015e36a83cb5da30497fcee
Victor <me.victorhermes@gmail.com>
2019-08-21 18:27:24 -0300

    mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -14,14 +14,6 @@ class Database {
     this.mongo();
   }
 
-  init() {
-    this.connection = new Sequelize(databaseConfig);
-
-    models
-      .map(model => model.init(this.connection))
-      .map(model => model.associate && model.associate(this.connection.models));
-  }
-
   mongo() {
     this.mongoConnection = mongoose.connect(
       'mongodb://192.168.99.100:27017/nodebackend',
