50a823a5309312ff93bd9606ca1f7650a6bb315d
Victor <me.victorhermes@gmail.com>
2019-08-21 18:27:02 -0300

    mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -21,6 +21,13 @@ class Database {
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
   }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://192.168.99.100:27017/nodebackend',
+      { useNewUrlParser: true, useFindAndModify: true }
+    );
+  }
 }
 
 export default new Database();
