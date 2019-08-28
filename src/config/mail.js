b1c712ae26fa9f778bb98abe9f5245fa8b7707c7
Victor <me.victorhermes@gmail.com>
2019-08-28 18:02:43 -0300

    nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '3aca40b5a6e31b',
+    pass: '0686e2c7eb30a8',
+  },
+  default: {
+    from: 'Victor Hermes <noreply@moom.com.br>',
+  },
+};
