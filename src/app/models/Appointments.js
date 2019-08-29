6021e0fe0eb598d5cfe2e5d89bdeb7ad308d55df
Victor <me.victorhermes@gmail.com>
2019-08-29 20:55:36 -0300

    list appointments

--- src/app/models/Appointments.js
+++ src/app/models/Appointments.js
@@ -1,5 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
-import { isBefore } from 'date-fns';
+import { isBefore, subHours } from 'date-fns';
 
 class Appointments extends Model {
   static init(sequelize) {
@@ -11,6 +11,12 @@ class Appointments extends Model {
           type: Sequelize.VIRTUAL,
           get() {
             return isBefore(this.date, new Date());
+          },
+        },
+        cancelable: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(new Date(), subHours(this.date, 2));
           },
         },
       },
