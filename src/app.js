f949cea7fbf98c31c9081dc703712c395c5371c8
Victor <me.victorhermes@gmail.com>
2019-08-29 21:14:20 -0300

    youch

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
+import 'express-async-errors';
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import sentryConfig from './config/sentry';
 import routes from './routes';
@@ -14,6 +16,7 @@ class App {
 
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
@@ -28,6 +31,14 @@ class App {
   routes() {
     this.server.use(routes);
     this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
